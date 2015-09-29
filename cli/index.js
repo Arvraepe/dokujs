7f21600dd30e545f863ab6db2b185a49569337b7
Arne Van Raepenbusch <arvraepe@gmail.com>
2015-09-29 19:00:58 +0200

    changed from handlebars to dotjs

--- cli/index.js
+++ cli/index.js
@@ -1,22 +1,13 @@
 var fs = require('fs');
-var Handlebars = require('handlebars');
-
-Handlebars.registerHelper("forObject",function(obj,options) {
-    if (!obj) return [];
-    var arr = Object.keys(obj).filter(function (key) { return key.charAt(0) !== '$'; } );
-    return arr.map(function(key,index) {
-        obj[key].$name = key;
-        obj[key].$index = index;
-        obj[key].$first = index === 0;
-        obj[key].$last  = index === arr.length-1;
-        return options.fn(obj[key]);
-    }).join('');
-});
+var dots = require("dot");
 
 var data = fs.readFileSync(__dirname+'/examples/example-service.json');
-var source = fs.readFileSync(__dirname+'/templates/api.handlebars').toString();
+var source = fs.readFileSync(__dirname+'/templates/api.dot').toString();
 
-var template = Handlebars.compile(source);
+console.log(source);
+
+var template = dots.template(source);
+
 var result = template({ api: JSON.parse(data) });
 
 fs.writeFileSync(__dirname+'/output/api.js', result);
